e8c2a343101ac11b185700c7334ebd4c36813226
Andreiskate2 <andrei.ungur@mail.mcgill.ca>
2017-11-17 22:18:34 -0500

    Data can be returned from db

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 from app import create_app
+from flask import jsonify
 import sqlalchemy
+from sqlalchemy.sql import text
 
-CONN = sqlalchemy.create_engine('sqlite:///test.db')
+CONN = sqlalchemy.create_engine('sqlite:///data.db')
 app = create_app("development")
 
 @app.route('/')
@@ -12,5 +14,17 @@ def index():
 def hello_world():
     return 'Ok!'
 
+#10116
+@app.route('/player/<int:player_id>')
+def get_player(player_id):
+    fields = ["player_id","full_name","minutes","pull_up_field_goal_percentage"]
+    return select_with_id(fields, player_id)
+
+def select_with_id(fields, player_id):
+    query = text('select '+", ".join(fields)+' from player_stats where player_id = :p')
+    player = CONN.execute(query, p=player_id).fetchone()
+    player_dict = { key:value for key, value in zip(fields, player) }
+    return jsonify(player_dict)
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True)
